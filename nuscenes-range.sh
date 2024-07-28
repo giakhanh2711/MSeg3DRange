c26d33f9cfda18fd8ec25a19e4fcd026c09d1557
Le Gia Khanh <53686667+giakhanh2711@users.noreply.github.com>
2024-07-28 15:35:05 +0700

    Update nuscenes-range.sh

--- nuscenes-range.sh
+++ nuscenes-range.sh
@@ -5,6 +5,6 @@ source ./.bashrc.local
 
 
 # 1 gpu: non-distributed train0
-CUDA_VISIBLE_DEVICES=0 python ./tools/train.py configs/semanticnusc/MSeg3D/semnusc_range_48_e36.py \
-	--resume_from work_dirs/semnusc_range_48_e36/latest.pth \
+CUDA_VISIBLE_DEVICES=0 python ./tools/train.py configs/semanticnusc/MSeg3D/semnusc_range_48_e50.py \
+	--resume_from work_dirs/semnusc_range_48_e50/latest.pth \
 	--validate
